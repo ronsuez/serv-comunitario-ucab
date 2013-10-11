785fcff784e3fb837fc132536c98760d4c477513
root <root@rsuez-pc>
2013-10-11 12:43:42 -0430

    doing some modifications on to prestador's view
    .

---
+++ mvc/application/views/prestador/ge_prestador.php
@@ -0,0 +1,19 @@
+<ul class="nav nav-tabs" id="myTab">
+  <li class="active"><a href="#home">Home</a></li>
+  <li><a href="#profile">Profile</a></li>
+  <li><a href="#messages">Messages</a></li>
+  <li><a href="#settings">Settings</a></li>
+</ul>
+
+<div class="tab-content">
+  <div class="tab-pane active" id="home">...</div>
+  <div class="tab-pane" id="profile">...</div>
+  <div class="tab-pane" id="messages">...</div>
+  <div class="tab-pane" id="settings">...</div>
+</div>
+
+<script>
+  $(function () {
+    $('#myTab').tab();
+  });
+</script>
