6860199f6a60d3c12132f5ee9679af0fd1376de2
root <root@rsuez-pc>
2013-10-11 13:36:53 -0430

    doing some modifications on to ge_prestador's view
    .

--- mvc/application/views/prestador/ge_prestador.php
+++ mvc/application/views/prestador/ge_prestador.php
@@ -33,81 +33,9 @@
 </ul>
 
 <div class="tab-content">
-  <div class="tab-pane" id="home">home</div>
-  <div class="tab-pane active" id="profile">
-	<div class="container">
-		<div class="panel panel-default">
-			<div class="panel-body">
-				<div class="input-group input-group-sm">
-					<input type="text" class="form-control" placeholder="Introduzca nombre o cédula del prestador">
-					<span class="input-group-btn">
-						<button class="btn btn-default" type="button"><span class="glyphicon glyphicon-search"></span></button>
-					</span>
-				</div><!-- /input-group -->
-				<br>
-				<div class="form-group">
-					<label for="">Nombres</label>
-					<input type="text" class="form-control" placeholder="Introduzca los nombres del prestador">
-				</div>
-				<div class="form-group">
-					<label for="">Apellidos</label>
-					<input type="text" class="form-control" placeholder="Introduzca los apellidos del prestador">
-				</div>
-
-				<!-- Datos personales -->
-				<div class="panel panel-info">
-					<div class="panel-heading">Datos Personales</div>
-					<div class="panel-body">
-						<div class="form-group">
-							<label>Correo electrónico</label>
-							<input type="text" class="form-control" placeholder="Introduzca el correo electrónico">
-						</div>
-						<div class="form-group">	
-							<label for="">Teléfono celular</label>
-							<input type="text" class="form-control" placeholder="Introduzca un teléfono celular">
-						</div>
-						<div class="form-group">
-							<label for="">Teléfono de habitación</label>
-							<input type="text" class="form-control" placeholder="Introduzca un teléfono habitación">
-						</div>
-						<div class="form-group">
-							<label for="">Direccion de habitación</label>
-							<input type="text" class="form-control" placeholder="Introduzca una direccion de habitación">
-						</div>
-					</div>
-				</div>
-
-				<!-- Datos academicos -->
-				<div class="panel panel-info">
-					<div class="panel-heading">Datos Académicos</div>
-					<div class="panel-body">
-						<div class="form-group">
-							<label for="">Numero de expediente</label>
-							<input type="text" class="form-control" placeholder="Introduzca el numero de expediente">
-						</div>
-						<div class="form-group">
-							<label for="">Escuela</label>
-							<input type="text" class="form-control" placeholder="Introduzca el nombre de la escuela">
-						</div>
-						<div class="form-group">
-							<label for="">Mención</label>
-							<input type="text" class="form-control" placeholder="Introduzca la mención">
-						</div>
-					</div>
-				</div>
-
-				<!-- Foto -->
-				<form enctype="multipart/form-data" action="uploader.php" method="POST">
-					<label for="">Foto: </label>
-					<input name="uploadedfile" type="file">
-				</form> 
-			</div><!-- /panel-body -->
-		</div><!-- /panel panel-default -->
-	</div><!-- /container -->
-</div>
-  <div class="tab-pane" id="messages">messages</div>
-</div>
-
+  <div class="tab-pane active " id="home">home</div>
+  <div class="tab-pane" id="profile"></div>
+  
 <script>
   $(function () {
     $('#myTab').tab();
