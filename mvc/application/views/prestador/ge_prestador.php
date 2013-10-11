5c4610d57c9c36c44d3bca9ed3a9b3b1b2e5fe70
root <root@rsuez-pc>
2013-10-11 13:01:29 -0430

    doing some modifications on to ge_prestador's view
    .

--- mvc/application/views/prestador/ge_prestador.php
+++ mvc/application/views/prestador/ge_prestador.php
@@ -1,13 +1,13 @@
 <ul class="nav nav-tabs" id="myTab">
   <li class="active"><a href="#home">Inscribir</a></li>
-  <li><a href="#profile">Consultar modificar</a></li>
+  <li><a href="#profile" data-url="insertar_prestador">Consultar modificar</a></li>
   <li><a href="#messages">Reportes</a></li>
 </ul>
 
 <div class="tab-content">
-  <div class="tab-pane active" id="home">...</div>
-  <div class="tab-pane" id="profile">...</div>
-  <div class="tab-pane" id="messages">...</div>
+  <div class="tab-pane active" id="home">home</div>
+  <div class="tab-pane" id="profile">profile</div>
+  <div class="tab-pane" id="messages">messages</div>
 </div>
 
 <script>
