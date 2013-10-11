6c4286139d9c1e2a5b3b7c9ab7cd60b2cf9af3de
root <root@rsuez-pc>
2013-10-11 13:18:24 -0430

    doing some modifications on to ge_prestador's view
    .

--- mvc/application/views/prestador/ge_prestador.php
+++ mvc/application/views/prestador/ge_prestador.php
@@ -1,3 +1,31 @@
+
+        <script>
+                  
+                  $(document).ready(function() {
+                  
+        $('#myTab a').click(function (e) {
+            e.preventDefault();
+            
+            var url = $(this).attr("data-url");
+              var href = this.hash;
+              var pane = $(this);
+            
+            // ajax load from data-url
+            $(href).load(url,function(result){      
+                pane.tab('show');
+            });
+          });
+
+          // load first tab content
+          $('#home').load($('.active a').attr("data-url"),function(result){
+            $('.active a').tab('show');
+          });
+
+        
+        });
+        
+        </script>
+
 <ul class="nav nav-tabs" id="myTab">
   <li class="active"><a href="#home">Inscribir</a></li>
   <li><a href="#profile" data-url="insertar_prestador">Consultar modificar</a></li>
