991fa72470ff992b6c2a8502e8f7a5db5e3709c2
root <root@rsuez-pc>
2013-10-12 12:00:56 -0430

    doing some modifications on to config file .

--- mvc/application/config/config.php
+++ mvc/application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/sc/mvc/';
+$config['base_url']	= 'http://localhost/servcom/mvc/';
 
 /*
 |--------------------------------------------------------------------------
