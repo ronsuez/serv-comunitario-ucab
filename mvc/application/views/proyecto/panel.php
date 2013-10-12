95486a32ccee4aeffb24dda101594932ea92b254
root <root@rsuez-pc>
2013-10-12 12:33:41 -0430

    doing some modifications on to database and proyecto_panel view
    .

--- mvc/application/views/proyecto/panel.php
+++ mvc/application/views/proyecto/panel.php
@@ -6,8 +6,8 @@
 			margin-top:30px;
 		}
 </style>
-		<div class="container">
-			<div class="header">
+
+        <div class="header">
 				<h1>Gestión de Proyectos</h1>
 				<ul class="nav nav-tabs" id="tabs">
 					<li class="active"><a href="#" id="b_crear">Crear</a></li>
@@ -16,28 +16,41 @@
 				</ul>
 			</div>
 
+
+		<div class="container">
+			
 			<div class="main-content" id="crear">
-				<h2>General</h2>
-				<div class="jumbotron">
-					<div class="form-group">
-						<label for="titulo_proyecto">Título del proyecto</label>
-		  				<input type="text" class="form-control" id="titulo_proyecto">
-		  			</div>
-		  			<div class="form-group">
-		  				<label for="suscribe">Organización o comunidad quien suscribe convenio</label>
-		  				<select class="form-control" id="suscribe">
-		  					<option value="">Seleccione</option>
-						</select>
-					</div>
-					<div class="form-group">
-		  				<label for="ejecuta">Organización o comunidad donde se ejecuta el proyecto</label>
-		  				<select class="form-control" id="ejecuta">
-		  					<option value="">Seleccione</option>
-						</select>
-					</div>
-				</div>
-				
-				<div>
+                            
+			<div class="panel panel-default">
+			 <div class="panel-heading"> Datos basicos del proyecto </div>
+                                <div class="panel-body">
+
+                                               <div class="form-group">
+                                                       <label for="titulo_proyecto">Título del proyecto</label>
+                                                       <input type="text" class="form-control" id="titulo_proyecto">
+                                               </div>
+                                               <div class="form-group">
+                                                       <label for="suscribe">Organización o comunidad quien suscribe convenio</label>
+                                                       <select class="form-control" id="suscribe">
+                                                               <option value="">Seleccione</option>
+                                                       </select>
+                                               </div>
+                                               <div class="form-group">
+                                                       <label for="ejecuta">Organización o comunidad donde se ejecuta el proyecto</label>
+                                                       <select class="form-control" id="ejecuta">
+                                                               <option value="">Seleccione</option>
+                                                       </select>
+                                               </div>
+                               </div>
+
+                       
+			</div>
+                            
+                            <div class="panel panel-default">
+                                    <div class="panel-heading"> Descripcion del proyecto </div>
+                                <div class="panel-body">
+
+                            
 					<div class="form-group">
 						<label for="descripcion">Descripción</label>
 						<textarea class="form-control" id="descripcion" placeholder="Diagnóstico de la situación, justificación, impacto."></textarea>
@@ -54,6 +67,11 @@
 			  	<br><input type="submit" class="btn btn-default" id="enviar" value="Crear Proyecto"></input><br><br>
 			</div>	
 
+                            
+                            
+                          </div> <!--/tab-crear-->
+                    
+                    
 			<div class="main-content" id="consultar_modificar">
 					
 
