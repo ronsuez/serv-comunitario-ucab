35f739ba8b59eff5188748c429753568d6a99d46
ronsuez <rsuez93@gmail.com>
2013-10-16 00:26:02 -0430

    Update config.php

--- mvc/application/config/config.php
+++ mvc/application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/projects/serv-comunitario-ucab/mvc/';
+$config['base_url']	= 'http://servcom-ucab.rsuezdev.com/sc/mvc/';
 
 /*
 |--------------------------------------------------------------------------
