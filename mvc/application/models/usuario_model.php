e4f084aa839b958a561594bd774a6e6b792c3d59
oscarb92 <osalbr.14@gmail.com>
2013-11-12 13:18:22 -0430

    se creo el modelo de usuarios

---
+++ mvc/application/models/usuario_model.php
@@ -0,0 +1,51 @@
+<?php class Usuario_model extends CI_Model {
+
+
+    function __construct()
+    {
+        // Call the Model constructor
+        parent::__construct();
+
+        
+        $this->load->database();
+
+
+    }
+
+
+public function check_user($user,$pass){
+
+    
+    $query = $this->db->query("SELECT cedula,tipo FROM  usuario WHERE cedula = '$user' AND contrasena = '$pass' ");
+
+      $salida = $query->result_array();
+
+                if ($query->num_rows() > 0)
+                {
+
+                    return $salida;
+            
+                }else{
+
+                return -1;
+                }
+                    
+        
+        }
+
+        public function get_info_user($id,$tipo){
+
+
+                if($tipo == "CO"){
+
+                 $query=$this->db->query("SELECT * FROM coordinador WHERE ci='$id'");
+                
+                }
+
+                return $query->result_array();          
+
+        }
+
+    
+
+}
