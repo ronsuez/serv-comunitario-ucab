262038e67d12a626efa50a7140b99078907d0b3d
JuanPareles <juanpareles@gmail.com>
2013-11-11 23:06:44 -0430

    Cambios para la insercion de datos en la bd

--- mvc/application/models/prestador_model.php
+++ mvc/application/models/prestador_model.php
@@ -13,36 +13,40 @@
     }
 
 
-public function listar_datos_prestador($cedula){
+    public function listar_datos_prestador($cedula){
 
-            /* Query Datos Personales  */
-            $query1 = $this->db->query("SELECT ci, nombre, apellido, email, celular, telefono, direccion 
-                                        FROM  `prestador` 
-                                        WHERE ci = $cedula");
+        /* Query Datos Personales  */
+        $query1 = $this->db->query("SELECT ci, nombre, apellido, email, celular, telefono, direccion 
+            FROM  `prestador` 
+            WHERE ci = $cedula");
 
-            /* Query Datos Academicos */
-            $query2 = $this->db->query("SELECT no_exp, escuela, semestre, mencion
-                                        FROM  `prestador` 
-                                        WHERE ci = $cedula");
+        /* Query Datos Academicos */
+        $query2 = $this->db->query("SELECT no_exp, escuela, semestre, mencion
+            FROM  `prestador` 
+            WHERE ci = $cedula");
 
-            $salida =  array('estado'=>"1",'datos_personales' =>$query1->result_array(),'datos_academicos' =>$query2->result_array());
+        $salida = $query1->result_array();
 
+        if ($query1->num_rows() > 0 || $query2->num_rows() > 0)
+        {
 
-
-                if ($query1->num_rows() > 0 || $query2->num_rows() > 0)
-                {
-
-                    return $salida;
+            return $salida;
             
-                }else{
-
-                    $salida =  array('estado' =>"-1");    
-                    
-                    return $salida;
-                }
-                    
-        
         }
+        else {
+
+            return -1;
+        }
+
+        
+    }
+
+
+    public function insertar_datos($nombre, $apellido, $celular, $email, $telefono, $direccion, $expediente, $escuela, $mencion)
+    {
+        // ci nombre  Apellido    email   celular telefono    no_exp  escuela semestre    mencion direccion
+        $query1 = $this->db->query("INSERT INTO prestador VALUES (999, '$nombre', '$apellido', '$email', $celular, $telefono, $expediente, '$escuela', 1, '$mencion', '$direccion')");
+    }
 
     
 
