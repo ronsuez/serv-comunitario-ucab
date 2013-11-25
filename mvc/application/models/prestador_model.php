e4a1add5a8ec28c2fe00b2731c16463d78f2d9f1
Miya09 <mireya.od9@gmail.com>
2013-11-25 02:05:22 -0430

    modelo-funcionalidad busquedas ajax v3

--- mvc/application/models/prestador_model.php
+++ mvc/application/models/prestador_model.php
@@ -41,31 +41,41 @@ public function listar_datos_prestador($cedula){
                     return $salida;
                 }
                     
+        
         }
-           
-   
-    public function insertar_datos_prestador($datos){
-        $query = $this->db->insert('prestador', array(
-                'ci_prestador' => $datos["cedula"],
-                'nombre_prestador' => $datos["nombre"],
-                'Apellido_prestador' => $datos["apellido"],
-                'email_prestador' => $datos["email"],
-                'celular_prestador' => $datos["celular"],
-                'telefono_prestador' => $datos["telefono"],
-                'no_exp_prestador' => $datos["expediente"],
-                'escuela_prestador' => $datos["escuela"],
-                'semestre_prestador' => $datos["semestre"],
-                'mencion_prestador' => "1",
-                'direccion_prestador' => $datos["direccion"]));
 
 
-            if($query){
-                return "0";
+public function buscar_prestador($q,$o){
+
+            if($o== "cedula"){
+                $filtro="ci_prestador";
+                  $sql="SELECT ci_prestador, nombre_prestador, Apellido_prestador
+                                        FROM prestador 
+                                        WHERE $filtro=$q";
+
+            }else if($o=="nombre"){
+                $filtro="nombre_prestador";
+                  $sql="SELECT ci_prestador, nombre_prestador, Apellido_prestador
+                                        FROM prestador 
+                                        WHERE $filtro LIKE '".$q."%'
+                                        OR Apellido_prestador LIKE '".$q."%'";
             }
-            else{
+
+        
+
+            $query = $this->db->query($sql);
+
+            if($query->num_rows()>0){
+
+                return $query->result_array();
+            }else{
+
                 return "-1";
             }
-    }  
+   
+}
+
+
 
 public function listar_x_pro($id_proyecto){
 
@@ -92,11 +102,37 @@ public function listar_x_pro($id_proyecto){
                     return $salida;
                 }
                   
-
-
 }        
 
+public function buscar_proyectos_prestador($ci_prestador){
+        $sql = "SELECT proyecto.id_proyecto, proyecto.nombre_proyecto
+                FROM   participa, proyecto  
+                WHERE  participa.ci_prestador = $ci_prestador 
+                AND    proyecto.id_proyecto   = participa.id_proyecto";
+
+        $query=$this->db->query($sql,$ci_prestador);
+
+
+             if ($query->num_rows() > 0)
+                {
+
+                    return $query->result_array();
+            
+                }else{
+
+                    $salida =  "-1";    
+                    
+                    return $salida;
+                }
 }
+
+
+}    
+
+
+
+
+
 
 /*SELECT Nombre
 FROM  `prestador` 
@@ -106,4 +142,3 @@ SELECT Apellido
 FROM  `prestador` 
 WHERE CI_prestador =24541299
 */
-
