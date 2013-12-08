7ce0f57f5a394e9d2a4281fa818e0809585bbce1
oscar <osalbr.14@gmail.com>
2013-12-08 02:53:52 -0430

    reporte proyecto actualizado

--- mvc/application/views/proyecto/ver_proyecto.php
+++ mvc/application/views/proyecto/ver_proyecto.php
@@ -29,127 +29,118 @@
      		 });
 
 	});
+
+	$("#reporte").hide();
 		
 </script>
 
 <div id="reporte">
 
-	<table id="tabla-header">
-	  <tr>
-	  	<td>
-			<div style="float: left;"><img src=<?php echo base_url()."application/views/proyecto/images/ucab.jpg";?> alt="ucab" height="90" width="90"></div>
-	  	</td>
-	   	<td>
-	   		<p>Universidad Catolica Andres Bello</p>
-			<p>Prologancion Av. Atlantico - Puerto Ordaz</p>
-			<p>Teléfonos: 0286-­9230230, 0286-9232358</p>
-			<p><?php echo $escuela;?></p>
-		</td>
-	  	<td>
-	  		<p>Coordinación de Proyección a la Comunidad</p>
-			<p>Servicio Comunitario Universitario</p>
-			<p>Período Académico 2009 – 2010<p>
-	  	</td>
-	 	<td>
-	 		<div><img src=<?php echo base_url()."application/views/proyecto/images/proyeccion.png";?>  alt="proyeccion" height="90" width="90"></div>
-	 	</td>
-	 </tr>
+	<div id="tabla-header">
+		<img src=<?php echo base_url()."application/views/proyecto/images/ucab.jpg";?> alt="ucab" height="79" width="378">
+	</div>
+
+	<table id="contenido" border="1" >
+		<tbody>	
+			<tr>
+				<td><span class="about-info">Titulo del proyecto</span></td>
+				<td><?=$datos[0]["nombre_proyecto"]?></td>	
+			</tr>
+			<tr>
+				<td>
+					<span class="about-info">Organización o comunidad que suscribe el 
+						Convenio</span>
+					</td>
+				<td><?=$datos[0]["ci_coord"]?></td>	
+			</tr>
+			<tr>
+				<td><span class="about-info">Estado</span></td>
+				<td><?=$datos[0]["estado_proyecto"]?></td>	
+			</tr>
+				
+			<tr>
+				<td><span class="about-info">Persona responsable en la organización o 
+				    comunidad que suscribe el Convenio</span>
+				</td>
+				<td><?=$datos[0]["ci_asesor"]?></td>	
+			</tr>	
+			<tr>
+				<td><span class="about-info">Dirección, teléfonos y correo electrónico de la
+					 organización o comunidad que suscribe el Convenio 
+				</span>
+				</td>
+				<td> Por asignar</td>	
+			</tr>	
+			<tr>
+				<td>
+					Organización o comunidad con la que se 
+					ejecutará el proyecto 
+				</td>
+
+				<td>Varias unidades productivas</td>	
+			</tr>	
+			<tr>
+				<td><span class="about-info">Persona responsable de la organización o 
+					comunidad con la que se ejecutará el proyecto.
+					</span></td>
+				<td>Representantes de varias unidades productivas</td>	
+			</tr>	
+			<tr>
+				<td><span class="about-info">Lugar de ejecución del proyecto</span></td>
+				<td>Por asignar</td>	
+			</tr>
+			<!--data grosso del proyecto-->
+			<tr>
+				<td><span class="about-info">Diagnostico</span></td>
+				<td><?=$datos[0]["diagnostico_proyecto"]?></td>	
+			</tr>
+			<tr>
+				<td><span class="about-info">Justificacion</span></td>
+				<td><?=$datos[0]["justificacion_proyecto"]?></td>	
+			</tr>
+			<tr>
+				<td><span class="about-info">Impacto</span></td>
+				<td><?=$datos[0]["impacto_proyecto"]?></td>	
+			</tr>
+			<tr>
+				<td><span class="about-info">Objetivo General</span></td>
+				<td><?=$datos[0]["obj_generales_proyecto"]?></td>	
+			</tr>
+			<tr>
+				<td><span class="about-info">Objetivos Especificos</span></td>
+				<td><?=$datos[0]["obj_especificos_proyecto"]?></td>	
+			</tr>
+			<tr>
+				<td><span class="about-info">Metas</span></td>
+				<td><?=$datos[0]["metas_proyecto"]?></td>	
+			</tr>
+			<tr>
+				<td><span class="about-info">Producto</span></td>
+				<td><?=$datos[0]["producto_proyecto"]?></td>	
+			</tr>
+				<tr>
+				<td><span class="about-info">Plan de trabajo</span></td>
+				<td><?=$datos[0]["plan_trabajo_proyecto"]?></td>	
+			</tr>
+			<tr>
+				<td><span class="about-info">Recursos</span></td>
+				<td><?=$datos[0]["recursos_requeridos_proyecto"]?></td>	
+			</tr>
+			<tr>
+				<td><span class="about-info">Cronograma</span></td>
+				<td><?=$datos[0]["cronograma_proyecto"]?></td>	
+			</tr>
+				
+				
+		</tbody>	
 	</table>
 
-<table  border="1" >
-	<tbody>	
-		<tr>
-			<td><span class="about-info">Titulo del proyecto</span></td>
-			<td><?=$datos[0]["nombre_proyecto"]?></td>	
-		</tr>
-		<tr>
-			<td>
-				<span class="about-info">Organización o comunidad que suscribe el 
-					Convenio</span>
-				</td>
-			<td><?=$datos[0]["ci_coord"]?></td>	
-		</tr>
-		<tr>
-			<td><span class="about-info">Estado</span></td>
-			<td><?=$datos[0]["estado_proyecto"]?></td>	
-		</tr>
-			
-		<tr>
-			<td><span class="about-info">Persona responsable en la organización o 
-			    comunidad que suscribe el Convenio</span>
-			</td>
-			<td><?=$datos[0]["ci_asesor"]?></td>	
-		</tr>	
-		<tr>
-			<td><span class="about-info">Dirección, teléfonos y correo electrónico de la
-				 organización o comunidad que suscribe el Convenio 
-			</span>
-			</td>
-			<td> Por asignar</td>	
-		</tr>	
-		<tr>
-			<td>
-				Organización o comunidad con la que se 
-				ejecutará el proyecto 
-			</td>
-
-			<td>Varias unidades productivas</td>	
-		</tr>	
-		<tr>
-			<td><span class="about-info">Persona responsable de la organización o 
-				comunidad con la que se ejecutará el proyecto.
-				</span></td>
-			<td>Representantes de varias unidades productivas</td>	
-		</tr>	
-		<tr>
-			<td><span class="about-info">Lugar de ejecución del proyecto</span></td>
-			<td>Por asignar</td>	
-		</tr>
-		<!--data grosso del proyecto-->
-		<tr>
-			<td><span class="about-info">Diagnostico</span></td>
-			<td><?=$datos[0]["diagnostico_proyecto"]?></td>	
-		</tr>
-		<tr>
-			<td><span class="about-info">Justificacion</span></td>
-			<td><?=$datos[0]["justificacion_proyecto"]?></td>	
-		</tr>
-		<tr>
-			<td><span class="about-info">Impacto</span></td>
-			<td><?=$datos[0]["impacto_proyecto"]?></td>	
-		</tr>
-		<tr>
-			<td><span class="about-info">Objetivo General</span></td>
-			<td><?=$datos[0]["obj_generales_proyecto"]?></td>	
-		</tr>
-		<tr>
-			<td><span class="about-info">Objetivos Especificos</span></td>
-			<td><?=$datos[0]["obj_especificos_proyecto"]?></td>	
-		</tr>
-		<tr>
-			<td><span class="about-info">Metas</span></td>
-			<td><?=$datos[0]["metas_proyecto"]?></td>	
-		</tr>
-		<tr>
-			<td><span class="about-info">Producto</span></td>
-			<td><?=$datos[0]["producto_proyecto"]?></td>	
-		</tr>
-			<tr>
-			<td><span class="about-info">Plan de trabajo</span></td>
-			<td><?=$datos[0]["plan_trabajo_proyecto"]?></td>	
-		</tr>
-		<tr>
-			<td><span class="about-info">Recursos</span></td>
-			<td><?=$datos[0]["recursos_requeridos_proyecto"]?></td>	
-		</tr>
-		<tr>
-			<td><span class="about-info">Cronograma</span></td>
-			<td><?=$datos[0]["cronograma_proyecto"]?></td>	
-		</tr>
-			
-			
-	</tbody>	
-</table>
+	<div id="firmas">
+		<h3>Validación del Proyecto</h3>
+		<div>Coordinador Servicio Comunitario Escuela</div>
+		<div>Coordinador Servicio Proyección a la Comunidad</div>
+		<div>Responsable por la Comunidad</div>
+	</div>
 
 </div>
 
