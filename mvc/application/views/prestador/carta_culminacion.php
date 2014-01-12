0feee4978e972c48cd0ff8775fb660dffee09599
Ronsuez <rsuez93@gmail.com>
2014-01-12 17:04:38 -0430

    vistas faltantes para los reportes

---
+++ mvc/application/views/prestador/carta_culminacion.php
@@ -0,0 +1,44 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+     
+  <link href="<?php echo base_url() ; ?>application/views/css/carta_culminacion.css" rel="stylesheet">
+  
+    </head>
+    
+<?php
+
+    $datos_p = get_object_vars($datos);
+
+    $fecha = "a los ".date("d")." días del mes de ".date("m")." de ".date("Y");
+ ?>
+
+    <body>
+       <div id="contenedor">
+           <div id="cabecera"> 
+           <h6>codigo</h6>
+           <img src=<?php echo base_url()."application/views/proyecto/images/ucab.jpg";?> alt="ucab" height="90" width="90"/>
+           </div>
+           
+       <div id="contenido">
+           <h2>CONSTANCIA DE CULMINACION DEL SERVICIO COMUNITARIO DEL ESTUDIANTE DE EDUCACION SUPERIOR</h2>
+
+           <p>Quien suscribe, hacen constar que el/la bachiller <u> <?=$datos_p['nombre_prestador']." ".$datos_p['apellido_prestador'];?> 
+
+            </u>expediente Nº <u> <?php echo $datos_p['no_exp_prestador']?> </u> titular de la 
+            Cédula de Identidad Nº<u> <?=$datos_p['ci_prestador']?></u> 
+            estudiante de la Escuela de <u> <?=$datos_p['escuela_prestador']?> </u>, 
+            ha cumplido satisfactoriamente con las 120 horas de servicio, 
+            así como con los objetivos y actividades previstos en el Plan de trabajo del proyecto ___________________________________________________. con el cual finalizó su Servicio Comunitario.</p>
+          <p>Igualmente se hace constar que sus actuaciones han sido acordes a lo establecido en la Ley del Servicio Comunitario del 
+            Estudiante de Eduacion Superior publicada en Gaceta Oficial Nº 38.272 del 14 de septiembre de 2005 y en el Reglamento del Servicio Comunitario del Estudiante de Educacion Superior, aprobado por el Consejo Universitario de la Universidad Católica Andres Bello.</p>
+           <p>En Ciudad Guayana, <?php echo $fecha ?></p>
+       </div>
+       <div id="pie">
+           
+           <p>__________________________________ Coordinador(a) de Servicio Comunitario del Estudiante de Educacion Superior</p>
+           <p id="final">Escuela de ____________________</p>
+       </div>
+       </div>
+    </body>
+</html>
---
+++ mvc/application/views/prestador/notificacion_culminacion.php
@@ -0,0 +1,108 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+        <style>
+        
+            
+            #contenedor{
+                
+                
+                width:972px;
+                margin:auto;
+                border-radius: 8px;
+            }
+            
+            #cabecera{
+                margin:20px auto;     
+            }
+            
+            #cabecera img{
+            margin: 0px 0px 0px 60px;
+            width:300px;
+            height:50px;
+            }
+            
+            #cabecera h6{
+                margin: 0px 200px 0px 0px;
+                float:right;
+                position:relative;
+            }
+            
+            #contenido{
+            
+            margin:80px auto;
+            /*width:500px;*/
+            height:auto;   
+            }
+            
+            #contenido h2{
+                font-size: 15px;
+                margin:auto;
+                text-align:center;
+                width:500px;
+                height:50px;
+            }
+            
+            #contenido #fecha{
+                
+                margin: 0px 185px 0px 0px;
+                width:auto;
+                height:auto;
+                float:right;
+            }
+            
+            #contenido p{
+                
+                text-align:justify;
+                margin:auto;
+                width:600px;
+                height:auto;
+                line-height:40px;
+                
+            }
+            #pie p{
+                width:300px;
+                height:50px;
+                text-align:center;
+                margin:auto;
+            }
+            
+            #pie #final{
+                width:300px;
+                height:50px;
+                text-align:center;
+                margin:auto;
+                line-height:50px;
+            }
+            
+        
+        </style>
+    </head>
+    
+    <body>
+       <div id="contenedor">
+           <div id="cabecera"> 
+           <h6>codigo</h6>
+           <img src="logo.jpg"/>
+           </div>
+           
+       <div id="contenido">
+           <h2>NOTIFICACION DE CULMINACION DEL SERVICIO COMUNITARIO</h2>
+           
+            <p id="fecha">Ciudad Guayana, _____de ________ de 20__</p>
+            <p>Sres. _____________</p>
+            <p>Atn. ______________</p>
+            <br>
+            <p>Reciba un cordial saludo en la oportunidad de notificarle la culminación del Proyecto de la Ley de Servicio Comunitario del Estudiante de Educacion Superior titulado __________________________________, por parte de los estudiantes ____________________________ de la carrera de  __________________________</p>
+            <p>Agradecemos ampliamente toda la colaboración y atención dispensada a nuestros estudiantes durante el período de ejecución de dicho proyecto, lo cual permitió un acercamiento con la institución y la comunidad, así como la oportunidad de aplicar los conocimientos adquiridos durante la carrera, al servicio de los demás.</p>
+            <p>Adjunto a la presente _______________________________, resultantes del proyecto ejecutado.</p>
+            <p>Quedamos a sus ordenes,atentamente.</p>
+       </div>
+       <div id="pie">
+           
+           <p>__________________________________ Coordinador(a) de Servicio Comunitario del Estudiante de Educacion Superior</p>
+           <p id="final">Escuela de ____________________</p>
+       </div>
+       </div>
+    </body>
+</html>
