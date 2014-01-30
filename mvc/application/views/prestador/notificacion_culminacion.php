c2164d64a49b96a2120a0f4ac14cd13caaa3e74a
oscar <osalbr.14@gmail.com>
2014-01-30 08:39:28 -0430

    recuperando el config

--- mvc/application/views/prestador/notificacion_culminacion.php
+++ mvc/application/views/prestador/notificacion_culminacion.php
@@ -10,6 +10,10 @@
 
     $datos_p = get_object_vars($datos);
     $datos_Co = get_object_vars($nombre);
+    $datos_comunidad = $info_comunidad; 
+    $dato_asesor = $nombre_asesor;
+
+
     
     //print_r($nombre);
 
@@ -28,12 +32,12 @@
            <h2>NOTIFICACION DE CULMINACION DEL SERVICIO COMUNITARIO</h2>
            
             <p id="fecha"><?php echo $fecha ?></p>
-            <p>Sres. _____________</p>
-            <p>Atn. ______________</p>
+            <p>Sres. <u><? echo $datos_comunidad[0]['nombre_localidad']?></u></p>
+            <p>Atn. <u><? echo $dato_asesor[0]['nombre_asesor']?></u></p>
             <br>
             <p>Reciba un cordial saludo en la oportunidad de notificarle la culminación del Proyecto de la Ley de Servicio Comunitario del Estudiante de Educacion Superior titulado <u> <?=$nombre_pro[0]['nombre_proyecto']?></u>, por parte de los estudiantes  de la carrera de <u> <?=$datos_p['escuela_prestador']?> </u></p>
             <p>Agradecemos ampliamente toda la colaboración y atención dispensada a nuestros estudiantes durante el período de ejecución de dicho proyecto, lo cual permitió un acercamiento con la institución y la comunidad, así como la oportunidad de aplicar los conocimientos adquiridos durante la carrera, al servicio de los demás.</p>
-            <p>Adjunto a la presente _______________________________, resultantes del proyecto ejecutado.</p>
+            <p>Adjunto a la presente, resultantes del proyecto ejecutado.</p>
             <p>Quedamos a sus ordenes,atentamente.</p>
        </div>
        <div id="pie">
